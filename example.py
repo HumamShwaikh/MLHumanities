4224a657bc7e1be917556631746ce19dd0c146ee
HumamShwaikh <humamshwaikh101@gmail.com>
2019-11-27 16:09:33 -0500

    Added all the sub parts

--- example.py
+++ example.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-
-sentence = "Nobody ever stopped him in the street to say, with gladsome looks, \"My dear Scrooge, how are you? When will you come to see me?\" No beggars implored him to bestow a trifle, no children asked him what it was o’clock, no man or woman ever once in all his life inquired the way to such and such a place, of Scrooge. Even the blind men’s dogs appeared to know him; and when they saw him coming on, would tug their owners into doorways and up courts; and then would wag their tails as though they said, \"No eye at all is better than an evil eye, dark master!\""
+file = open('stave1.txt',mode='r',encoding='UTF8')
+STAVE = file.read()
+file.close()
 
-#Extract First Word
+#Talk to Jacob first
+#905 920 5553 for Merge Conflicts
+#Extract Each Paragraphs
+PARAGRAPHS = STAVE.split('\n')
 
-sentences = sentence.find("Scrodoge")
-print(sentences)
+#Find speech in the parameter 'paragraph' return a array
+#Richard
+def findSpoken(paragraph): 
+    return {}
+
+#Return a list of everytime word appears
+#Cedrick
+def getAllOccurance(word, section):
+    myList = section.find(word)
+    
+    return myList
+
+#Remove all punctuation 
+#Jack
+def cleanStave():
+    newStave = STAVE
+    
+    return newStave
+
+print(getAllOccurance("Scrooge", STAVE))
