ee2d795837ebb7d17b9f36f78a8b222a6b2a62a6
adelonte <adelonte@gmail.com>
2019-11-28 15:47:01 -0500

    getAllOccurence function added

--- example.py
+++ example.py
@@ -1,31 +1,51 @@
+import re
+
+
 # -*- coding: utf-8 -*-
-file = open('stave1.txt',mode='r',encoding='UTF8')
+file = open('stave1.txt', 'r')
 STAVE = file.read()
 file.close()
 
 #Talk to Jacob first
 #905 920 5553 for Merge Conflicts
 #Extract Each Paragraphs
+
 PARAGRAPHS = STAVE.split('\n')
 
 #Find speech in the parameter 'paragraph' return a array
 #Richard
-def findSpoken(paragraph): 
+
+
+def findSpoken(paragraph):
     return {}
 
+
 #Return a list of everytime word appears
-#Cedrick
+#Returns the starting index of the word in a list
+
+
 def getAllOccurance(word, section):
-    myList = section.find(word)
-    
-    return myList
+
+    list = []
+
+    for i in re.finditer(word, section):
+        list.append(i.start())
+
+    return list
+
+
 
 #Remove all punctuation 
 #Jack
+
+
 def cleanStave():
     newStave = STAVE
     
     return newStave
 
-print(getAllOccurance("Scrooge", STAVE))
+
+
+
+print(getAllOccurance("Marley", STAVE))
 
