392861bf77f43ee1fbdb7ec6c884f40f43eaae7d
jksza042 <55514862+jksza042@users.noreply.github.com>
2019-12-02 04:04:17 -0500

    Created getAllQuotes
    Created the getAllQuotes function. After some testing I couldn't make a reliable algorithm to determine the speaker (mostly due to the prevalance of Scrooge's name in the text and spacy not recognizing relational titles such as Scrooge's Nephew). getAllQuotes is complete except for that functionality

--- staveML.py
+++ staveML.py
@@ -19,8 +19,37 @@ file.close()
 def getConfidence(entity, documentEntities):
     return 0.0 
 
-##  getAllQuotes(stave) returns list[quote, name]
+##  getAllQuotes(stave, persList) returns list[quote, name]
 #   Jacob
+#Given the writing style of the book, determining a name based on proximity to the quote is impossible. The functionality is taken out for now
+def getAllQuotes(stave):
+    '''
+    (str) -> (list)
+    Given a paragraph of text, finds the quotes, appends them to a list, and returns them
+    Preconditions: Non quotation text must not be enclosed by quotations (This actually does happen for the first element of the sample list)
+    '''
+
+    stave = stave.replace('“','"')
+    stave = stave.replace('”','"')
+    quoteSplit = stave.split('"')
+    quotes = []
+    quoteStart = 1
+    if stave[0] == '\"':
+        quoteStart = 0
+
+
+    for quote in range(quoteStart,len(quoteSplit),2):
+
+        #speaker = "Unknown"
+        #for person in persList:
+            #if person in quoteSplit[quote+1]:
+             #   speaker = person
+            #elif person in quoteSplit[quote-1]:
+            #    speaker = person
+
+        quotes.append(quoteSplit[quote])
+
+    return quotes
 
 ##  getSentiment(quote) returns signed float
 #   Amy (Lin)
@@ -39,7 +68,9 @@ print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
 print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
 
 # Find named entities, phrases and concepts
+
 for entity in doc.ents:
     print(entity.text, entity.label_)
+
 
 ## end Main
