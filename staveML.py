cd59f21d1935d3b85d0d5805a6daa2528c96232e
KateAThornley <43188335+KateAThornley@users.noreply.github.com>
2019-12-02 21:56:01 -0500

    added getConfidence function, finds the confidence level of the passed entity based on the most frequently attributed label

--- staveML.py
+++ staveML.py
@@ -2,6 +2,7 @@
 # python -m spacy download en_core_web_sm
 
 import spacy
+import operator
 
 # Load English tokenizer, tagger, parser, NER and word vectors
 nlp = spacy.load("en_core_web_sm")
@@ -12,7 +13,8 @@ text = file.read()
 doc = nlp(text)
 file.close()
 
-#make list of tuples from doc.ents and their attributes
+#make list of tuples from doc.ents, 
+# type of each tuple is (string entity name,string entity label)
 lst = []
 def makeTupleList():
     for entity in doc.ents:
@@ -21,7 +23,8 @@ def makeTupleList():
 lst = makeTupleList()
 
 
-#define dictionary of tuple entities where keys are names
+# define dictionary of tuple entities where keys are nouns, 
+# values per key are lists of noun's associations to text
 def convert(entityTuples, entityDictionary):
     for x, y in entityTuples:
         entityDictionary.setdefault(x, []).append(y)
@@ -31,11 +34,22 @@ entityDict = {}
 entityDict = convert(lst, entityDict)
 
 ##  getConfidence()
-#   Returns a float 0 to 1 as a ratio of how many times
+#   Returns a float 0 to 1 as a percentage of how many times
 #   the entity appeared with final type.
 #   Kat
 def getConfidence(entity, documentEntities):
-    return 0.0 
+
+    #create list of labels for passed entity
+    entityNounList = documentEntities[entity]
+    #create dictionary with key as labels and values as number of occurances per label for passed entity
+    counterDict = {i:entityNounList.count(i) for i in entityNounList}
+    #find the entity label with the highest occurance number
+    maxKey = max(counterDict.items(), key=operator.itemgetter(1))[0]
+    #total number of times labels were attributed to entity
+    totalMentionLabels = sum(counterDict.values())
+    maxEntOccur = counterDict.get(maxKey)
+    
+    return (maxEntOccur/totalMentionLabels)
 
 ##  getAllQuotes(stave) returns list[quote, name]
 #   Jacob
@@ -53,13 +67,19 @@ def getConfidence(entity, documentEntities):
 #   Humam
 
 # Analyze syntax
-#print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
-#print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
 
 # Find named entities, phrases and concepts
-#for entity in doc.ents:
-   # print(entity.text, entity.label_)
+for entity in doc.ents:
+   print(entity.text, entity.label_)
 
-#print(entityDict)
+#Show Entity Dictionary
 print(entityDict)
+
+#Get Confidence test
+conf = 0
+conf = getConfidence("Scrooge",entityDict)
+print(conf)
+
 ## end Main
