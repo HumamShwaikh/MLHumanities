d329b00ca03b956cd5c15cff5ff61d35653ccc8e
jksza042 <55514862+jksza042@users.noreply.github.com>
2019-12-03 01:26:55 -0500

    Finalized getAllQuotes
    Created a rudementary algorithm to determine the most likely speaker based on the closest name to the quote

--- staveML.py
+++ staveML.py
@@ -3,6 +3,7 @@
 
 import spacy
 import operator
+import random
 
 # Load English tokenizer, tagger, parser, NER and word vectors
 nlp = spacy.load("en_core_web_sm")
@@ -53,33 +54,44 @@ def getConfidence(entity, documentEntities):
 
 ##  getAllQuotes(stave, persList) returns list[quote, name]
 #   Jacob
-#Given the writing style of the book, determining a name based on proximity to the quote is impossible. The functionality is taken out for now
-def getAllQuotes(stave):
+def getAllQuotes(stave,persList):
     '''
-    (str) -> (list)
-    Given a paragraph of text, finds the quotes, appends them to a list, and returns them
-    Preconditions: Non quotation text must not be enclosed by quotations (This actually does happen for the first element of the sample list)
+    (str), (list) -> (list of tuples)
+    Given a paragraph of text, finds the quotes and appends them to a list along with the speaker of the quote, and returns the list
+    Preconditions: Non quotation text must not be enclosed by quotations (This actually does happen for the first element of the sample list), persList must have all the character's names, 
+    and the speaker's name must be the closest name to the quote
     '''
 
+    #Formats the stave for proper processing and initializes an accumulator variable for our return list
     stave = stave.replace('“','"')
     stave = stave.replace('”','"')
     quoteSplit = stave.split('"')
     quotes = []
+
+    #Checks if the text starts with a quote
     quoteStart = 1
     if stave[0] == '\"':
         quoteStart = 0
 
-
+    #Appends each quote to the list quotes and guesses the speaker by determining the name in closest proximity to the quote
     for quote in range(quoteStart,len(quoteSplit),2):
 
-        #speaker = "Unknown"
-        #for person in persList:
-            #if person in quoteSplit[quote+1]:
-             #   speaker = person
-            #elif person in quoteSplit[quote-1]:
-            #    speaker = person
+        speaker = "Unknown"
+        deltaSpeakerPos = 10000
+        for person in persList:
+            person = str(person)
+            distNextCell = len(quoteSplit[quote+1]) - quoteSplit[quote+1].find(person)
+            distPreviousCell = quoteSplit[quote-1].find(person)
 
-        quotes.append(quoteSplit[quote])
+            if person in quoteSplit[quote+1] and distNextCell < deltaSpeakerPos:
+                speaker = person
+                deltaSpeakerPos = distNextCell
+
+            if person in quoteSplit[quote-1] and distPreviousCell < deltaSpeakerPos:
+                speaker = person
+                deltaSpeakerPos = distPreviousCell
+
+        quotes.append((quoteSplit[quote],speaker))
 
     return quotes
 
@@ -104,6 +116,13 @@ print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
 for entity in doc.ents:
    print(entity.text, entity.label_)
 
+# Creates a list of people for use in getAllQuotes
+persList = []
+for entity in doc.ents:
+    if entity.label_ == "PERSON" and str(entity) not in persList:
+        persList.append(str(entity))
+
+
 #Show Entity Dictionary
 print("\nThe following is the dictionary of entities:\n")
 print(entityDict)
@@ -114,5 +133,9 @@ conf = 0
 conf = getConfidence("Scrooge",entityDict)
 print(conf)
 
+#getAllQuotes Test
+print("\nText and speaker for a random quote: ")
+quotes = getAllQuotes(text,persList)
+print(quotes[random.randint(0,len(quotes))])
 
 ## end Main
